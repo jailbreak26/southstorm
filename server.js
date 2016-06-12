dbeb023af70fdc1fad5577a88f54399ba04604b3
jailbreak26 <jailbreak31@gmail.com>
2016-06-12 21:56:37 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,44 +1,10 @@
 #!/usr/bin/env node
-var parse = require('url').parse;
 
-var data = '<?xml version="1.0"?>' +
-  '<!DOCTYPE cross-domain-policy SYSTEM "http://www.adobe.com/xml/dtds/cross-domain-policy.dtd">' +
-  '<cross-domain-policy>' +
-  '<site-control permitted-cross-domain-policies="none"/>' +
-  '</cross-domain-policy>';
-
-module.exports = function crossdomain(options) {
-
-  options = options || {};
-  var caseSensitive = options.caseSensitive;
-
-  return function crossdomain(req, res, next) {
-
-    var pathname = parse(req.url).pathname;
-
-    var uri;
-    if (caseSensitive) {
-      uri = pathname;
-    } else {
-      uri = pathname.toLowerCase();
-    }
-
-    if ('/crossdomain.xml' === uri) {
-      res.writeHead(200, {
-        'Content-Type': 'application/xml'
-      });
-      res.end(data);
-    } else {
-      next();
-    }
-
-  };
-
-};
-
+var host = process.env.PORT ? '0.0.0.0' : '127.0.0.1';
 var port = process.env.PORT || 1337;
 
 var cors_proxy = require('cors-anywhere');
+var cors_proxy = require('scratchx-crossdomain');
 
 cors_proxy.createServer({
     originWhitelist: [],
