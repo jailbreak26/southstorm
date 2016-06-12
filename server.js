71463483b60b2eeb1d049e72ffc6fc16e123ef28
jailbreak26 <jailbreak31@gmail.com>
2016-06-12 19:24:59 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
+var http = require('http'),
+	request = require('request'),
+	fs = require('fs'),
+	domain = require('domain'),
+	favicon = require('zlib').gzipSync(fs.readFileSync('favicon.ico'))
+	crossdomainXML = require('zlib').gzipSync(fs.readFileSync('crossdomain.xml')),
+	requestOptions = {
+		encoding: null,
+		rejectUnauthorized: false,
+		headers: {
+			'accept-encoding': 'identity'
+		}
+	},
 var host = process.env.PORT ? '0.0.0.0' : '127.0.0.1';
 var port = process.env.PORT || 1337;
 
