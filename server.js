b92d10386ccd90482cebce87a8e171ac856a3934
jailbreak26 <jailbreak31@gmail.com>
2016-06-12 21:27:50 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,29 @@
+#!/usr/bin/env node
+var http = require('http')
+var request = require('request')
+var fs = require('fs');
+var index = fs.readFileSync('index.html');
+
 var host = process.env.PORT ? '0.0.0.0' : '127.0.0.1';
 var port = process.env.PORT || 1337;
+
+http.createServer(function (req, res) {
+  console.log(req.url);
+  res.setTimeout(25000)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  if (req.url == "/"){
+  	res.writeHead(200);
+  	res.write(index);
+  	res.end();
+  }else{
+  try {
+    request(req.url.slice(1), {encoding: null}, function(error, response, body) {
+      res.write(body)
+      res.end()
+    })
+  }
+  catch(e) {}}
+}).listen(port)
 
 var cors_proxy = require('cors-anywhere');
 
