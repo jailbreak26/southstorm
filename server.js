1eec51540c76ab64c7f2bb5f2fdfa6f45de72d35
jailbreak26 <jailbreak31@gmail.com>
2016-08-08 16:37:22 +0800

    Update server.js

--- server.js
+++ server.js
@@ -17,5 +17,62 @@ var http = require('http'),
 	fs = require('fs'),
 	domain = require('domain'),
 	favicon = require('zlib').gzipSync(fs.readFileSync('favicon.ico'))
-	crossdomainXML = require('zlib').gzipSync(fs.readFileSync('crossdomain.xml')),
-
+	crossdomainXML = require('zlib').gzipSync(fs.readFileSync('crossdomain.xml'))
+	allowedOriginalHeaders = new RegExp('^' + require('./allowedOriginalHeaders.json').join('|'), 'i')
+	requestOptions = {
+		encoding: null,
+		rejectUnauthorized: false,
+		headers: {
+			'accept-encoding': 'identity'
+		}
+	},
+	handler = function handler(req, res) {
+		switch (req.url) {
+			case "/":
+			case "/favicon.ico":
+				res.setHeader('content-encoding', 'gzip')
+				res.setHeader('content-type', 'image/x-icon')
+				res.writeHead(200);
+				res.write(favicon);
+				res.end();
+				break;
+			case "/favicon.png":
+				res.setHeader('content-encoding', 'gzip')
+				res.setHeader('content-type', 'image/png')
+				res.writeHead(200);
+				res.write(faviconPNG);
+				res.end();
+				break;
+			case "/crossdomain.xml":
+				res.setHeader('content-encoding', 'gzip')
+				res.setHeader('content-type', 'application/xml')
+				res.writeHead(200);
+				res.write(crossdomainXML);
+				res.end();
+				break;
+			default:
+				if (bannedUrls.test(req.url)) {
+					res.writeHead(403);
+					res.end('FORBIDDEN');
+				} else {
+				try {
+					res.setTimeout(25000);
+					res.setHeader('Access-Control-Allow-Origin', '*');
+					res.setHeader('Access-Control-Allow-Credentials', false);
+					res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+					res.setHeader('Expires', new Date(Date.now() + 86400000).toUTCString()); // one day in the future
+					var r = request(req.url.slice(1), requestOptions);
+					r.pipefilter = function(response, dest) {
+						for (var header in response.headers) {
+							if (!allowedOriginalHeaders.test(header)) {
+								dest.removeHeader(header);	
+							}
+						}
+					};
+					r.pipe(res);
+				} catch (e) {
+					res.end('Error: ' +  ((e instanceof TypeError) ? "make sure your URL is correct" : String(e)));
+				}
+			}
+		}
+	}
