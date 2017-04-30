f6153a30eabc1524a962aa55bf0c59218bc04964
jailbreak26 <jailbreak31@gmail.com>
2017-04-30 11:37:48 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 #!/usr/bin/env node
 var host = process.env.PORT ? '0.0.0.0' : '127.0.0.1';
-var port = process.env.PORT || 1337;
-
+var port = process.env.PORT || 8080;
+ 
 var cors_proxy = require('cors-anywhere');
-
 cors_proxy.createServer({
-    originWhitelist: [],
+    originWhitelist: [], // Allow all origins 
     requireHeader: [],
     removeHeaders: ['cookie', 'cookie2']
-}).listen(port, host, function () {
+}).listen(port, host, function() {
     console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
+
+if (location.host === 'crossdomain.xml') { res.writeHead(200, {'Content-Type': 'application/xmll'}); var xml = ''; res.end(xmll); return; }
