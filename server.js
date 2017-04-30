c1ff9d0c952b7b52ac8c983323f2de1b98090791
jailbreak26 <jailbreak31@gmail.com>
2017-04-30 09:25:59 +0800

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,27 @@ var host = process.env.PORT ? '0.0.0.0' : '127.0.0.1';
 var port = process.env.PORT || 1337;
 
 var cors_proxy = require('cors-anywhere');
-
+var http = require('http'),
+	request = require('request'),
+	fs = require('fs'),
+	domain = require('domain'),
+	crossdomainXML = require('zlib').gzipSync(fs.readFileSync('crossdomain.xml'))
+	requestOptions = {
+		encoding: null
+	},
+	handler = function handler(req, res) {
+		switch (req.url) {
+			case "/":
+			case "/crossdomain.xml":
+				res.setHeader('content-encoding', 'gzip')
+				res.setHeader('content-type', 'application/xml')
+				res.writeHead(200);
+				res.write(crossdomainXML);
+				res.end();
+				break;		
+			
+		}
+	},
 cors_proxy.createServer({
     originWhitelist: [],
     requireHeader: [],
