fa9c81911bfff9a5b006b11c8daebeb33e8205ce
jailbreak26 <jailbreak31@gmail.com>
2017-04-30 11:20:16 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 #!/usr/bin/env node
+var host = process.env.PORT ? '0.0.0.0' : '127.0.0.1';
+var port = process.env.PORT || 1337;
+
 var cors_proxy = require('cors-anywhere');
 
 cors_proxy.createServer({
