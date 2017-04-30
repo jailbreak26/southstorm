3392277877b13c2a8317150598f331d84dd23ecc
jailbreak26 <jailbreak31@gmail.com>
2017-04-30 10:50:29 +0800

    Update server.js

--- server.js
+++ server.js
@@ -12,4 +12,15 @@ cors_proxy.createServer({
     console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
 
+var fs = require('fs');
+require('http').createServer(function(req, res) {
+  if (req.url === '/crossdomain.xml') {
+    fs.createReadStream('crossdomain.xml').pipe(res);
+    return;
+  }
+  // Let the server handle it
+  cors_proxy.emit('request', req, res);
+}).listen(8080); // Listen on port 8080.
+
+
 
