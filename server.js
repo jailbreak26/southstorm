2b38f5aa2a75c4944d5f8e6746986f1d90ca2141
jailbreak26 <jailbreak31@gmail.com>
2017-04-30 09:33:21 +0800

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var host = process.env.PORT ? '0.0.0.0' : '127.0.0.1';
 var port = process.env.PORT || 1337;
 
 var cors_proxy = require('cors-anywhere');
-var http = require('http'),
+var cors_proxy = require('crossdomain.xml'),
 	request = require('request'),
 	fs = require('fs'),
 	domain = require('domain'),
@@ -23,7 +23,7 @@ var http = require('http'),
 				break;		
 			
 		}
-	},
+	};
 cors_proxy.createServer({
     originWhitelist: [],
     requireHeader: [],
