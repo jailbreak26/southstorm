816512d93df1b2ccac3d2927dd34119261b005e2
jailbreak26 <jailbreak31@gmail.com>
2017-10-25 15:27:01 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,6 @@
 var host = process.env.PORT ? '0.0.0.0' : '127.0.0.1';
 var port = process.env.PORT || 1337;
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 var cors_proxy = require('cors-anywhere');
 cors_proxy.createServer({
     originWhitelist: [], // Allow all origins 
